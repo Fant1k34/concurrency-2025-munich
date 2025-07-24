527750c822f334a2e744f3dcf83837b745e7b5af
Nikita Dukin <nikita.dukin@jetbrains.com>
2025-07-24 10:06:41 +0200

    feat: implement msqueue

--- src/day1/MSQueue.kt
+++ src/day1/MSQueue.kt
@@ -13,11 +13,49 @@ class MSQueue<E> : Queue<E> {
     }
 
     override fun enqueue(element: E) {
-        TODO("implement me")
+        val newNode = Node(element)
+
+        while (true) {
+            // Запоминаем хвостовой элемент
+            val currentTail = tail.get()
+
+            // Хвост -- последний элемент, у которого ссылка на следующий равна null
+            // Если у currentTail и правда нет ссылки (к текущему моменту tail мог уже поменяться),
+            // то тогда currentTail остался актуальным,
+            // поэтому мы добавляем к нему атомарно ссылку на созданную ноду
+            if (currentTail.next.compareAndSet(null, newNode)) {
+                // Теперь заменяем значение Node внутри tail
+                tail.compareAndSet(currentTail, newNode)
+                return
+            } else {
+                // Если у tail есть ссылка на элемент, то тогда другой поток уже добавил ссылку на новую ноду
+                // Но не успел обновить хвост
+                // Поможем ему -- сделаем это за него и начнем сначала
+                tail.compareAndSet(currentTail, currentTail.next.get())
+            }
+        }
     }
 
     override fun dequeue(): E? {
-        TODO("implement me")
+        while (true) {
+            val currentHead = head.get()
+            val nextOfCurrentHead = currentHead.next.get()
+
+            // Если ссылка на следующий элемент отсутствует, то текущий элемент dummy, и нечего удалять
+            // Dummy -- обязятельная и единственная нода-пустышка, которая имеет значение null
+            if (nextOfCurrentHead == null) return null
+
+            // Если head не поменялся, то тогда заменяем его на следующий его элемент
+            if (head.compareAndSet(currentHead, nextOfCurrentHead)) {
+                // Запоминаем значение в следующем элементе
+                val result = nextOfCurrentHead.element
+
+                // Превращаем следующий элемент в пустышку
+                nextOfCurrentHead.element = null
+                return result
+            }
+            // Если head изменился, пробуем заного
+        }
     }
 
     // FOR TEST PURPOSE, DO NOT CHANGE IT.
